0171d849b76660642a1d2c14b636bd0e9de6599d
unknown <zhakulin.com>
2021-04-02 16:49:31 +0300

    Add Manager script

---
+++ Assets/Scriptes/Pin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    [SerializeField] float speed = 20f;
+    [SerializeField] Rigidbody2D rigidbody;
+    private bool isPinned = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isPinned == false)
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector2.up * speed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisionObj)
+    {
+        if (collisionObj.tag == "Pin")
+        {
+            Debug.Log("Game OVER!!!!");
+        }
+        else if (collisionObj.tag == "Rotator")
+        {
+            isPinned = true;
+            transform.SetParent(collisionObj.transform);
+        }
+    }
+}
